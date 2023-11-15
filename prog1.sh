d4ca267ea2c0f0d5f21bd3fec8ef75e8f846cb8b
unknown <arpit.sahu@bridgelabz.com>
2023-11-15 12:26:33 +0530

    Added: multiplication ; Refactored : The code for calculator

--- prog1.sh
+++ prog1.sh
@@ -1,6 +1,28 @@
-a=10
-b=20
-sum=$(($a+$b))
-sub=$(($a-$b))
-echo sub is $sub
-echo sum is $sum
+echo Welcome to the calculator
+echo "Enter Your 1st number"
+read a
+echo "Enter Your 2nd number"
+read b
+echo "Enter the choice:"
+echo "1: Addition"
+echo "2: Subtraction"
+echo "3 : Multiplication"
+read input 
+
+case $input in
+	1)
+		sum=$(($a+$b))
+		echo Sum = $sum
+		;;
+	2)
+		sub=$(($a-$b))
+		echo Sub = $sub
+		;;
+	3)
+		mul=$(($a*$b))
+		echo Mul = $mul
+		;;
+	*)
+		echo enter correct input
+		;;
+esac
